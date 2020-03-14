46fa678f6ac5ae978b6d63642caad073c1bc101e
simonalong <simonalong@126.com>
2020-03-14 11:46:33 +0800

    chg 修改项目名字

---
+++ src/main/java/com/simonalong/teddy/Teddy.java
@@ -0,0 +1,9 @@
+package com.simonalong.teddy;
+
+/**
+ * @author shizi
+ * @since 2020/3/14 上午11:38
+ */
+public class Teddy {
+
+}
---
+++ src/test/java/com/simonalong/teddy/BaseTest.java
@@ -0,0 +1,13 @@
+package com.simonalong.teddy;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+/**
+ * @author shizi
+ * @since 2020/3/14 上午11:39
+ */
+@RunWith(JUnit4.class)
+public class BaseTest {
+
+}

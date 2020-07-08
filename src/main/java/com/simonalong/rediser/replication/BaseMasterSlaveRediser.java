35a49acd42f484575f7dd622a35e054dbb01faef
simonalong <simonalong@126.com>
2020-07-08 18:09:28 +0800

    delete 删除集群模式中的多key的引用

--- src/main/java/com/simonalong/rediser/replication/BaseMasterSlaveRediser.java
+++ src/main/java/com/simonalong/rediser/replication/BaseMasterSlaveRediser.java
@@ -6,6 +6,6 @@ import com.simonalong.rediser.replication.core.*;
  * @author shizi
  * @since 2020/7/5 3:49 PM
  */
-public interface BaseMasterSlaveRediser extends MasterSlaveAdvancedJedisCommands, MasterSlaveBasicCommands, MasterSlaveHashHandler, MasterSlaveJedisCommands, MasterSlaveMultiKeyCommands, MasterSlaveObjectCommand, MasterSlaveObjectQuery, MasterSlaveScriptingCommands, MasterSlaveSelector {
+public interface BaseMasterSlaveRediser extends MasterSlaveAdvancedJedisCommands, MasterSlaveBasicCommands, MasterSlaveHashHandler, MasterSlaveJedisCommands, MasterSlaveObjectCommand, MasterSlaveObjectQuery, MasterSlaveScriptingCommands, MasterSlaveSelector {
 
 }
